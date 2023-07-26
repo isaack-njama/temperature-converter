ee4bc2ad46e518cf696a44ba56eec0b4cb9df164
Isaack Njama <munguiisaack@gmail.com>
2023-07-26 21:33:52 +0300

    feat: add functionality to convert to fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+/**
+ * Given temperature in degrees_celcius:
+ * 1. Compute degrees_fahrenheit using the formula: degrees_fahrenheit = degrees_celcius * 1.8000 + 32.00
+ */
+
+ fn main() {
+    convert_to_fahrenheit(-273.15)
+}
+
+// Given temperature in Celsius
+fn convert_to_fahrenheit(degrees_celsius: f32) {
+    let degrees_fahrenheit = degrees_celsius * 1.8000 + 32.00;
+
+    println!("The temperature in Degrees Fahrenheit is: {}", degrees_fahrenheit);
 }
